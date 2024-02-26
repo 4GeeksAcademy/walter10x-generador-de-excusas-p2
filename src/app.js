44d60ad5fdd483c6a25baa2473e911e4522fc3cd
Walter Rivero <127254776+walter10x@users.noreply.github.com>
2024-02-26 19:50:00 +0000

    gnerar mi primera excusa

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Array de opciones para cada parte de la excusa
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Genera índices aleatorios para cada array
+  let randomWhoIndex = Math.floor(Math.random() * who.length);
+  let randomActionIndex = Math.floor(Math.random() * action.length);
+  let randomWhatIndex = Math.floor(Math.random() * what.length);
+  let randomWhenIndex = Math.floor(Math.random() * when.length);
+
+  // Obtiene las partes aleatorias de la excusa
+  let randomWho = who[randomWhoIndex];
+  let randomAction = action[randomActionIndex];
+  let randomWhat = what[randomWhatIndex];
+  let randomWhen = when[randomWhenIndex];
+
+  // Construye la excusa
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  // Muestra la excusa dentro del elemento con el ID "excuse" en HTML
+  document.getElementById("excuse").innerText = excuse;
 };
